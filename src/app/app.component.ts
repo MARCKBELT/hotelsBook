d04b6b92484d038120a53e5fa6e7b5eb39be9371
marckbelt <marck.belt@gmail.com>
2024-11-26 17:31:50 -0400

    Primer Spring TErminado - Nav Bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TopHeaderComponent } from "./components/common/top-header/top-header.component";
+import { NavbarComponent } from './components/common/navbar/navbar.component';
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, TopHeaderComponent, NavbarComponent,]
 })
 export class AppComponent {
   title = 'hotelsBook';
